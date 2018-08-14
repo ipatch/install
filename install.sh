00aa64c62a2a585e10f36a3f509918afe291d381
ipatch <chris.r.jones.1983@gmail.com>
2018-08-13 19:03:37 -0500

    update  script to use local fork

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 umask 022
-which ruby >/dev/null || eval "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install-ruby`"
+which ruby >/dev/null || eval "`curl -fsSL https://raw.githubusercontent.com/ipatch/install/exp-linuxbrew-install/install-ruby`"
 exec ruby -e "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install`"
